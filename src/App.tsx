d0e4b1454b5ee166aac9eee4b098c3a86aa7a113
Fexyd <cararoja241@gmail.com>
2025-05-25 19:00:14 -0600

    yo soy yo 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import YO from "./assets/yo.jpeg";
+import Button from "./components/button";
 
 function App() {
   return (
     <>
       <img src={YO} alt="" />
       <h1>Soy yo</h1>
+      <Button></Button>
     </>
   );
 }
